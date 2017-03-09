b57b0d898355561a8045a75fe6c103924a8e643f
20170109 <arbogajk@login01.cluster.cs.jmu.edu>
2017-03-09 12:27:09 -0500

    removed omp get num threads from main.c (unneeded)

--- src/par/main.c
+++ src/par/main.c
@@ -65,9 +65,7 @@ int main( int argc, char** argv ) {
     MPI_Init(&argc, &argv);
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-#	ifdef _OPENMP
-	num_threads = omp_get_num_threads();
-#	endif	
+	
 	seed_random();
 	if (argc == 4) {
 		// Create shares -- "secret"  n  t 
